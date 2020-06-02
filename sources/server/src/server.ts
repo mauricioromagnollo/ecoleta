1a6e3cab3eff79b31938280f311f8201bc06e35f
x0n4d0 <romagnolimauricio@gmail.com>
2020-06-02 17:06:01 -0300

    feat: add express static and cors to server

--- sources/server/src/server.ts
+++ sources/server/src/server.ts
@@ -1,12 +1,17 @@
 import express from 'express';
+import cors from 'cors';
+import path from 'path';
 import routes from './routes';
 
 const APP_PORT = 3333;
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.listen(APP_PORT, () => {
   console.log(`[*] Server running on port: ${APP_PORT}`);
