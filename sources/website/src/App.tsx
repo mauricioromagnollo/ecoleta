a6bd327852d18573d2b6a58c41b42d480f522f42
x0n4d0 <romagnolimauricio@gmail.com>
2020-06-03 18:59:10 -0300

    feat: removed <h1> and added <Routes /> component

--- sources/website/src/App.tsx
+++ sources/website/src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
 
+import Routes from './routes';
+
 function App() {
   return (
-    <h1>Hello World</h1>
+    <div>
+      <Routes />
+    </div>
   );
 }
 
