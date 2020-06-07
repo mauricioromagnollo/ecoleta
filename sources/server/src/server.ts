02a1c66e3acaf875a8309061be24a8ee205e71a6
x0n4d0 <romagnolimauricio@gmail.com>
2020-06-07 19:03:12 -0300

    feat: added validation with celebrate/joi (#7)
    Added validation fields with celebrate and joi.
    
    Close #7

--- sources/server/src/server.ts
+++ sources/server/src/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import path from 'path';
 import routes from './routes';
+import { errors } from 'celebrate';
 
 const app = express();
 
@@ -10,6 +11,8 @@ app.use(express.json());
 app.use(routes);
 
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+
+app.use(errors());
 
 app.listen(process.env.SERVER_PORT, () => {
   console.log(`[*] Server running on port: ${process.env.SERVER_PORT}`);
