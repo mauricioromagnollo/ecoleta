e41ca54b8348b68711e14e1fd485af1ff0c8291c
x0n4d0 <romagnolimauricio@gmail.com>
2020-06-07 18:05:05 -0300

    feat: added image_url attribute to show image

--- sources/mobile/src/pages/Detail/index.tsx
+++ sources/mobile/src/pages/Detail/index.tsx
@@ -21,6 +21,7 @@ interface Params {
 interface Data {
   point: {
     image: string;
+    image_url: string;
     name: string;
     email: string;
     whatsapp: string;
@@ -73,7 +74,7 @@ const Detail = () => {
           <Icon name="arrow-left" size={20} color="#34cb79" />
         </TouchableOpacity>
 
-        <Image style={styles.pointImage} source={{ uri: data.point.image }} />
+        <Image style={styles.pointImage} source={{ uri: data.point.image_url }} />
 
         <Text style={styles.pointName}>{data.point.name}</Text>
         <Text style={styles.pointItems}>
--- sources/mobile/src/pages/Points/index.tsx
+++ sources/mobile/src/pages/Points/index.tsx
@@ -17,6 +17,7 @@ interface Item {
 interface Point {
   id: number;
   image: string;
+  image_url: string;
   name: string;
   latitude: number;
   longitude: number;
@@ -130,7 +131,7 @@ const Points = () => {
                   }} 
                 >
                   <View style={styles.mapMarkerContainer}>
-                    <Image style={styles.mapMarkerImage} source={{ uri: point.image }} />
+                    <Image style={styles.mapMarkerImage} source={{ uri: point.image_url }} />
                     <Text style={styles.mapMarkerTitle}>{point.name}</Text>
                   </View>
                 </Marker>
